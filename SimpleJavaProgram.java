ed7a5bf3e6cd3862d945b3edd4e1e970bd86b356
Rsandez <131881281+RaulSM20@users.noreply.github.com>
2024-05-14 17:25:56 +0200

    Update SimpleJavaProgram.java

--- SimpleJavaProgram.java
+++ SimpleJavaProgram.java
@@ -1,5 +1,6 @@
 public class SimpleJavaProgram {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo!");
+        System.out.println("Esta es una nueva línea para probar los cambios.");
     }
 }
